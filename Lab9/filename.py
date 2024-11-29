f7ddb5bcf32500967f56d3d01ca9ab13cad80a6e
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-11-29 10:01:32 +0000

    Image converted to grayscale. Plotted image to verify output.

--- Lab9/filename.py
+++ Lab9/filename.py
@@ -2,5 +2,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# ATU1 image loaded
+# Load the image
 mg = cv2.imread('ATU1.jpg',)
+mg = cv2.cvtColor(mg, cv2.COLOR_BGR2RGB)  # Fix colour issues
+
+# cvtColor() function to grayscale the image
+gray_image = cv2.cvtColor(mg, cv2.COLOR_BGR2GRAY)
+
+# Specify number of rows and columns
+nrows = 3
+ncols = 3
+
+# Originall
+plt.subplot(nrows, ncols, 1), plt.imshow(mg, cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+# Gray scale
+plt.subplot(nrows, ncols, 2), plt.imshow(gray_image, cmap='gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.show()
