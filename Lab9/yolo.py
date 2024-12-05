b04eebac1eb01b7d56854667f7396acfdf1725cd
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-12-05 22:14:54 +0000

    Counted unique objects.

--- Lab9/yolo.py
+++ Lab9/yolo.py
@@ -1,7 +1,22 @@
+import os
 from ultralytics import YOLO
+
+# Set the environment variable to avoid OpenMP runtime error
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 # Load a model
 model = YOLO("yolo11n.pt")  # load an official model
 
-# Predict with the model, focusing on a specific class (e.g., class 0 for 'person')
-results = model(source="vid.mp4", show=True, conf=0.4, save=True, classes=[0])  # predict on an image
+# Predict with the model
+# results = model(source="vid.mp4", show=True, conf=0.4, save=True)  # video
+# results = model(source="vid.mp4", show=True, conf=0.4, save=True, classes=[0])  # specific class
+
+results = model.track(source="vid.mp4", show=True, conf=0.4, save=True, classes=[2])
+
+# Count unique object detections
+unique_objects = set()
+for result in results:
+    for obj in result.boxes:
+        unique_objects.add(obj.id)
+
+print(f"Number of unique objects detected: {len(unique_objects)}")
