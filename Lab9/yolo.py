0c7d38601b412f6f0c4cb1d12c5165da90e875b3
Kyrylo Kozlovskyi <g00425385@atu.ie>
2024-12-05 21:58:51 +0000

    Constrained the algorithm to only detect people.

--- Lab9/yolo.py
+++ Lab9/yolo.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolo11n.pt")  # load an official model
 
-# Predict with the model
-results = model(source="vid.mp4", show=True, conf=0.4, save=True)  # predict on an image
+# Predict with the model, focusing on a specific class (e.g., class 0 for 'person')
+results = model(source="vid.mp4", show=True, conf=0.4, save=True, classes=[0])  # predict on an image
